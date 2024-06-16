9b43073ce89fa69f0c0ccefdbbdd108ed59a4f1d
khhan <jacob2572502@gmail.com>
2024-06-17 01:52:40 +0900

    imported useGetRecipeQuery endpoint hook for placeholding data for forms

--- src/pages/UpdateRecipe.tsx
+++ src/pages/UpdateRecipe.tsx
@@ -1,11 +1,28 @@
+import { useGetRecipeQuery } from "../services/recipe";
 import { useParams } from "react-router-dom";
 
 function UpdateRecipe() {
   let { recipeId } = useParams();
 
-  return (<div>
-    update {recipeId}
-  </div>)
-};
+  const {
+    data: recipeData,
+    error,
+    isLoading: recipeIsLoading,
+  } = useGetRecipeQuery(recipeId!);
+  if (recipeIsLoading) {
+    return <main className="text-foreground">Loading...</main>;
+  }
+  if (error) {
+    return <main className="text-foreground">Error!</main>;
+  }
+  if (recipeData) {
+    const { name, ingredients, directions } = recipeData;
+    console.log(recipeData);
+
+    return <div>update {recipeId}</div>;
+  }
+
+  return null;
+}
 
 export default UpdateRecipe;
