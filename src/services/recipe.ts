9d0491012a1506b3c0dd80441530d7a6f9bf77e3
khhan <jacob2572502@gmail.com>
2024-06-20 16:24:57 +0900

    added cache invalidation so that recipe list and edited recipe page can be updated when Recipe is created, updated, and deleted

--- src/services/recipe.ts
+++ src/services/recipe.ts
@@ -6,12 +6,21 @@ export const recipeApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://665951ecde346625136bf46a.mockapi.io/api/v1/",
   }),
+  tagTypes: ["Recipe"],
   endpoints: (builder) => ({
     getRecipes: builder.query<Recipe[], void>({
       query: () => "/recipes",
+      providesTags: (result) =>
+        result
+          ? [
+              { type: "Recipe", id: "LIST" },
+              ...result.map(({ id }) => ({ type: "Recipe" as const, id })),
+            ]
+          : [{ type: "Recipe", id: "LIST" }],
     }),
     getRecipe: builder.query<Recipe, string>({
       query: (recipeId) => `/recipes/${recipeId}`,
+      providesTags: (result, error, arg) => [{ type: "Recipe", id: arg }],
     }),
     addNewRecipe: builder.mutation<Recipe, Partial<Recipe>>({
       query: (recipeData) => ({
@@ -19,19 +28,28 @@ export const recipeApi = createApi({
         method: "POST",
         body: recipeData,
       }),
+      invalidatesTags: [{ type: "Recipe", id: "LIST" }],
     }),
     deleteRecipe: builder.mutation<Recipe, string>({
       query: (recipeId) => ({
         url: `/recipes/${recipeId}`,
         method: "DELETE",
       }),
+      invalidatesTags: [{ type: "Recipe", id: "LIST" }],
     }),
-    editRecipe: builder.mutation<Recipe, { recipeId: string; data: Partial<Recipe> }>({
+    editRecipe: builder.mutation<
+      Recipe,
+      { recipeId: string; data: Partial<Recipe> }
+    >({
       query: (recipe) => ({
         url: `/recipes/${recipe.recipeId}`,
         method: "PUT",
         body: recipe.data,
       }),
+      invalidatesTags: (result, error, arg) => [
+        { type: "Recipe", id: arg.recipeId },
+        { type: "Recipe", id: "LIST" },
+      ],
     }),
   }),
 });
